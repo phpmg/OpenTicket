1d8eff71a6221899b9370c903bff1bcf4a0273d9
Jansen Felipe <jansen.felipe@gmail.com>
2023-01-24 18:47:35 -0300

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <LoginPage />
+    </ThemeProvider>
   );
 }
 
