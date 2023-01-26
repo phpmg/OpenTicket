42db01939336b41835245acd4241b37aa1d1f069
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2023-01-26 20:57:27 -0300

    WIP criando api de usuario

---
+++ backend/app/Http/Controllers/Users/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController
+{
+    public function auth()
+    {
+    }
+
+    public function signOn(Request $request)
+    {
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Routing\Router;
 use App\Http\Controllers\Events\EventsController;
+use App\Http\Controllers\Users\UserController;
 
 /** @var Router $router */
 
@@ -15,5 +16,9 @@ $router->group([
 ], function () use ($router) {
     $router->group(['prefix' => 'events'], function () use ($router) {
         $router->get('/', [EventsController::class,"getEvents"]);
+    });
+    $router->group(['prefix' => 'users'], function () use ($router) {
+        $router->get('/auth', [UserController::class,"auth"]);
+        $router->post('/sign-on', [UserController::class,"signOn"]);
     });
 });
